40e05413139e4acb17b89ac6346a76747c13f1be
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-07-26 17:31:39 +0200

    fix(di): initialize Koin on iOS app startup

--- shared/src/iosMain/kotlin/io/nicolaszurbuchen/appname/MainViewController.kt
+++ shared/src/iosMain/kotlin/io/nicolaszurbuchen/appname/MainViewController.kt
@@ -2,5 +2,13 @@ package io.nicolaszurbuchen.appname
 
 import androidx.compose.ui.window.ComposeUIViewController
 import io.nicolaszurbuchen.appname.app.App
+import io.nicolaszurbuchen.appname.app.di.initKoin
+import org.koin.mp.KoinPlatform
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() =
+    ComposeUIViewController {
+        if (KoinPlatform.getKoinOrNull() == null) {
+            initKoin()
+        }
+        App()
+    }

99fc01924ebec85fa0a79de7fc2f45f4e97cb300
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-07-29 17:22:52 +0200

    test(pokemon-explorer): cover the four pokemon-explorer use cases

---
+++ shared/src/commonTest/kotlin/io/nicolaszurbuchen/appname/feature/pokemonexplorer/domain/usecase/ClearHistoryUseCaseTest.kt
@@ -0,0 +1,30 @@
+package io.nicolaszurbuchen.appname.feature.pokemonexplorer.domain.usecase
+
+import io.nicolaszurbuchen.appname.feature.pokemonexplorer.domain.fake.FakePokemonExplorerRepository
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class ClearHistoryUseCaseTest {
+
+    @Test
+    fun invoke_delegatesDirectlyToRepository() = runTest {
+        val repository = FakePokemonExplorerRepository()
+        val useCase = ClearHistoryUseCase(repository)
+
+        useCase()
+
+        assertEquals(1, repository.clearHistoryCallCount)
+    }
+
+    @Test
+    fun invoke_repositoryThrows_propagatesException() = runTest {
+        val repository = FakePokemonExplorerRepository().apply {
+            clearHistoryError = RuntimeException("disk full")
+        }
+        val useCase = ClearHistoryUseCase(repository)
+
+        assertFailsWith<RuntimeException> { useCase() }
+    }
+}

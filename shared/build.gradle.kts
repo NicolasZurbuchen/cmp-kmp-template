701c686f5e1b97eff3849e367f16dcd775e0ec13
Nicolas Zurbuchen <nicolas.zurbuchen@outlook.com>
2026-07-29 16:38:48 +0200

    build(pokemon-explorer): add turbine, ktor-client-mock, and sqldelight jdbc driver test deps

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -69,6 +69,9 @@ kotlin {
             implementation(libs.ktor.client.android)
             implementation(libs.ktor.client.okhttp)
         }
+        getByName("androidHostTest").dependencies {
+            implementation(libs.sqldelight.driver.jdbc.sqlite)
+        }
         commonMain.dependencies {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
@@ -95,6 +98,8 @@ kotlin {
         commonTest.dependencies {
             implementation(libs.kotlin.test)
             implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.ktor.client.mock)
+            implementation(libs.turbine)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
